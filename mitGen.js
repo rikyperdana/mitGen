fce6f4e2531e3fad87d5827685313970cad0ee43
Riky Perdana <rikyperdana@users.noreply.github.com>
2023-03-11 14:36:40 +0700

    Update mitGen.js

--- mitGen.js
+++ mitGen.js
@@ -1,15 +1,24 @@
-var m, _, mgState = {},
-withThis = (obj, cb) => cb(obj),
+var m, _, mgState = {}, comps = {}, state = {},
+withAs = (obj, cb) => cb(obj),
+ifit = (obj, cb) => Boolean(obj) && cb(obj),
+ands = array => array.reduce((a, b) => a && b, true),
+
 makeIconLabel = (icon, label) => [
   icon && m('span.icon', m('i.fas.fa-'+icon)),
   m('span', label)
 ],
+
+poster = (url, body, cb) => fetch(url, {
+  headers: {'Content-Type': 'application/json'},
+  method: 'post', body: JSON.stringify(body)
+}).then(res => res.json()).then(cb),
 
 mitGen = (obj, opts) => ({view: () => m('div',
   {class: 'has-background-light'},
   opts.theme && m('link', {rel: 'stylesheet', href:'https://unpkg.com/bulmaswatch/'+opts.theme+'/bulmaswatch.min.css'}),
   m('nav.navbar.is-primary.is-fixed-top',
     m('.navbar-brand',
+      obj.brand.opts,
       m('a.navbar-item',
         {onclick: () => [
           _.assign(mgState, {comp: obj.brand.comp}),
@@ -55,6 +64,7 @@ mitGen = (obj, opts) => ({view: () => m('div',
       m('.navbar-end', m('.navbar-item.has-dropdown.is-hoverable',
         m('a.navbar-link',
           {onclick: () => [
+            (mgState.burgerMenu = !mgState.burgerMenu),
             _.assign(mgState, {comp: obj.end.comp}),
             m.redraw()
           ]},
@@ -64,6 +74,7 @@ mitGen = (obj, opts) => ({view: () => m('div',
           obj.end.submenu && _.map(obj.end.submenu, (i, j) =>
             m('a.navbar-item',
               {onclick: () => [
+                (mgState.burgerMenu = !mgState.burgerMenu),
                 _.assign(mgState, {comp: i.comp}),
                 m.redraw()
               ]},
@@ -76,24 +87,29 @@ mitGen = (obj, opts) => ({view: () => m('div',
   ),
   m('section.section', m('.container',
     {style: 'min-height:100vh'},
-    m('.content', mgState.comp ? mgState.comp() : [
-      m('h1', 'Dashboard'),
-      _.chunk(_.map(obj.start, (v, k) => [v, k]), 3).map(i =>
-        m('.columns', i.map(j => m('.column',
-          m('a.box', m('article.media',
-            {onclick: () => [
-              _.assign(mgState, {comp: j[0].comp}),
-              m.redraw()
-            ]},
-            m('.media-left', m('span.icon.has-text-primary',
-              m('i.fas.fa-2x.fa-'+j[0].icon))
-            ),
-            m('.media-content', m('.content',
-              m('h3', j[0].full || _.startCase(j[1]))
+    withAs(
+      m('.content', mgState.comp ? mgState.comp() : [
+        m('h1', 'Dashboard'),
+        _.chunk(_.map(obj.start, (v, k) => [v, k]), 3).map(i =>
+          m('.columns', i.map(j => m('.column',
+            m('a.box', m('article.media',
+              {onclick: () => [
+                _.assign(mgState, {comp: j[0].comp}),
+                m.redraw()
+              ]},
+              m('.media-left', m('span.icon.has-text-primary',
+                m('i.fas.fa-2x.fa-'+j[0].icon))
+              ),
+              m('.media-content', m('.content',
+                m('h3', j[0].full || _.startCase(j[1]))
+              ))
             ))
-          ))
-        )))
-      )
-    ])
+          )))
+        )
+      ]),
+      body => opts.login ? (
+        opts.loginState ? body : opts.login()
+      ) : body
+    )
   ))
 )})
