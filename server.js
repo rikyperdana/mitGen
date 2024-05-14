cc592bc47baf7d8b7ca1f4db841ce09cd60a6bb4
Riky Perdana <perdanariky@gmail.com>
2024-05-15 04:21:02 +0700

    registration mechanic

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const
 io = require('socket.io'),
 express = require('express'),
+bcrypt = require('bcryptjs'),
 JSONdb = require('simple-json-db'),
-getColl = name => new JSONdb(`./db/${name}.json`),
+access = name => new JSONdb(
+  `./db/${name}.json`, {jsonSpaces: 2}
+),
+
+{parse, stringify} = JSON,
+withAs = (obj, cb) => cb(obj),
+randomId = x => Math.random().toString(36).slice(2),
 
 app = express()
   .use(express.static('public'))
@@ -11,11 +18,25 @@ app = express()
 io(app).on('connection', socket => [
 
   socket.on('dbCall', (obj, cb) => ({
-    json: _ => cb(getColl(obj.coll).JSON()),
-    get: _ => cb(getColl(obj.coll).get(obj.key)),
-    del: _ => cb(getColl(obj.coll).delete(obj.key)),
-    repl: _ => cb(getColl(obj.coll).JSON(obj.replace)),
-    set: _ => cb(getColl(obj.coll).set(obj.key, obj.value))
+    get: _ => cb(access(obj.coll).get(obj.key)),
+    del: _ => cb(access(obj.coll).delete(obj.key)),
+    set: _ => [access(obj.coll).set(obj.key, obj.value), cb(true)],
+    json: _ => cb(access(obj.coll).JSON()),
+    repl: _ => access(obj.coll).JSON(obj.replace)
   })[obj.action]()),
+
+  socket.on('register', (user, cb) => withAs(
+    Object.entries(access('users').JSON()).find(
+      i => i[1].username === user.username
+    ), foundUser => foundUser
+      ? cb({status: false, msg: 'User already registered.'})
+      : bcrypt.hash(`${user.password}`, 10, (err, hash) => withAs(
+        {...user, id: randomId(), password: hash},
+        newUser => [
+          access('users').set(newUser.id, newUser),
+          cb({...newUser, password: '*****'})
+        ]
+      ))
+  )),
 
 ])
