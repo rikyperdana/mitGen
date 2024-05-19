a00c19cec3f14be4075998183d4d35cbd0c61dd6
Riky Perdana <perdanariky@gmail.com>
2024-05-19 22:33:20 +0700

    require jsonDB

---
+++ deps/jsonDB.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+  set: (coll, key, data, cb) => fs.readFile(
+    `./db/${coll}.json`, 'utf8', (err, existing) =>
+    existing ? fs.writeFile( // find existing file
+      `./db/${coll}.json`, // if it's found
+      JSON.stringify({ // combine with the data
+        ...JSON.parse(existing), [key]: data
+      }, null, 2),
+      err => cb({status: !err})
+    ) : fs.writeFile( // if it doesn't
+      `./db/${coll}.json`, // just write a new one
+      JSON.stringify({[key]: data}, null, 2),
+      err => cb({status: !err})
+    )
+  ),
+
+  get: (coll, key, cb) => fs.readFile(
+    `./db/${coll}.json`, 'utf8', // get the specified key
+    (err, res) => res && cb(JSON.parse(res)[key])
+  ),
+
+  del: (coll, key, cb) => fs.readFile(
+    `./db/${coll}.json`, 'utf8', // get existing data
+    (err, res) => withAs(JSON.parse(res), existing => [
+      delete existing[key], fs.writeFile( // delete the pair
+        `./db/${coll}.json`, // overwrite the old json
+        JSON.stringify(existing, null, 2),
+        err => cb({status: !err})
+      )
+    ])
+  ),
+
+  all: (coll, cb) => fs.readFile(
+    `./db/${coll}.json`, 'utf8', // get all contents
+    (err, res) => res && cb(JSON.parse(res))
+  ),
+
+  rep: (coll, data, cb) => fs.writeFile(
+    `./db/${coll}.json`, // replace the entire content
+    JSON.stringify(data, null, 2),
+    err => cb({status: !err})
+  )
+}
--- server.js
+++ server.js
@@ -3,56 +3,10 @@ io = require('socket.io'),
 {nanoid} = require('nanoid'),
 express = require('express'),
 bcrypt = require('bcryptjs'),
-JSONdb = require('simple-json-db'),
-access = name => new JSONdb(`./db/${name}.json`),
+jsonDB = require('./deps/jsonDB.js'),
 
 {parse, stringify} = JSON, withAs = (obj, cb) => cb(obj),
 ands = arr => arr.reduce((acc, inc) => acc && inc, true),
-
-jsonDB = {
-
-  set: (coll, key, data, cb) => fs.readFile(
-    `./db/${coll}.json`, 'utf8', (err, existing) =>
-    existing ? fs.writeFile( // find existing file
-      `./db/${coll}.json`, // if it's found
-      JSON.stringify({ // combine with the data
-        ...JSON.parse(existing), [key]: data
-      }, null, 2),
-      err => cb({status: !err})
-    ) : fs.writeFile( // if it doesn't
-      `./db/${coll}.json`, // just write a new one
-      JSON.stringify({[key]: data}, null, 2),
-      err => cb({status: !err})
-    )
-  ),
-
-  get: (coll, key, cb) => fs.readFile(
-    `./db/${coll}.json`, 'utf8', // get the specified key
-    (err, res) => res && cb(JSON.parse(res)[key])
-  ),
-
-  del: (coll, key, cb) => fs.readFile(
-    `./db/${coll}.json`, 'utf8', // get existing data
-    (err, res) => withAs(JSON.parse(res), existing => [
-      delete existing[key], fs.writeFile( // delete the pair
-        `./db/${coll}.json`, // overwrite the old json
-        JSON.stringify(existing, null, 2),
-        err => cb({status: !err})
-      )
-    ])
-  ),
-
-  all: (coll, cb) => fs.readFile(
-    `./db/${coll}.json`, 'utf8', // get all contents
-    (err, res) => res && cb(JSON.parse(res))
-  ),
-
-  rep: (coll, data, cb) => fs.writeFile(
-    `./db/${coll}.json`, // replace the entire content
-    JSON.stringify(data, null, 2),
-    err => cb({status: !err})
-  )
-},
 
 app = express()
   .use(express.static('public'))
@@ -99,44 +53,17 @@ io(app).on('connection', socket => [
     )
   )),
 
-  socket.on('XgrantAccess', (admin, user, cb) => withAs(
-    // find the admin record first
-    Object.entries(access('users').JSON()).find(i => ands([
-      i[1].access.includes('superadmin'),
-      i[1].username === admin.username,
-      i[1].token === admin.token
-    ])), checkAdmin => checkAdmin && bcrypt.compare(
-      // is he really a super admin?
-      admin.password, checkAdmin[1].password,
-      (err, similar) => !similar
-        // if he isn't, then reject grantAccess
-        ? cb({status: false, msg: "You're not a super admin."})
-        : withAs(
-          // if he is, find the user to be granted access
-          Object.entries(access('users').JSON()).find(
-            i => i[1].username === user.username
-          ), grantUser => grantUser && [
-            // store the granted access in the user record
-            access('users').set(grantUser[1].id, {
-              ...grantUser[1], access: user.access
-            }),
-            // respond with successful changes
-            cb({status: true, msg: 'Access successfully granted.'})
-          ]
-        )
+  socket.on('grantAccess', (admin, user, cb) => jsonDB.all(
+    'users', allUsers => withAs(
+      Object.entries(allUsers).find(rec => ands([
+        rec[1].access.includes('superadmin'),
+        rec[1].username === admin.username,
+        rec[1].token === admin.token
+      ])), checkAdmin => checkAdmin && bcrypt.compare(
+        admin.password, checkAdmin[1].password, (err, equal) =>
+        equal ? jsonDB.set('users', user.id, user, cb)
+        : cb({status: false, msg: "You're not superadmin."})
+      )
     )
-  )),
-
-  /* ------------------------------------------------- */
-
-  /* DANGER ZONE, dev only
-  socket.on('dbCall', (obj, cb) => ({
-    get: _ => cb(access(obj.coll).get(obj.key)),
-    del: _ => cb(access(obj.coll).delete(obj.key)),
-    set: _ => [access(obj.coll).set(obj.key, obj.value), cb(true)],
-    json: _ => cb(access(obj.coll).JSON()),
-    repl: _ => access(obj.coll).JSON(obj.replace) // BUG
-  })[obj.action]()),
-  */
-
+  ))
 ])
