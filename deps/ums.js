28fe170be71535f39ea97ad4ad71caad31a4d9fd
Riky Perdana <perdanariky@gmail.com>
2024-05-20 18:28:52 +0700

    ums module

---
+++ deps/ums.js
@@ -0,0 +1,73 @@
+const fs = require('fs'),
+{nanoid} = require('nanoid'),
+io = require('socket.io'),
+bcrypt = require('bcryptjs'),
+jsonDB = require('./jsonDB.js'),
+
+withAs = (obj, cb) => cb(obj)
+
+module.exports = app =>
+io(app).of('/users').on('connection', socket => [
+
+  /* -------------- User Management System --------------- */
+
+  socket.on('signup', (user, cb) => jsonDB.all(
+    'users', allUsers => Object.entries(allUsers).find(
+      record => record[1].username === user.username
+    ) ? cb({status: false, msg: 'User already registered.'})
+    : bcrypt.hash(`${user.password}`, 10, (err, hash) => withAs(
+      nanoid(), id => jsonDB.set('users', id, {
+        ...user, id, password: hash, access: []
+      }, res => cb({status: true, msg: 'Registration successful.'}))
+    ))
+  )),
+
+  socket.on('signin', (user, cb) => jsonDB.all(
+    'users', allUsers => withAs(Object.entries(allUsers).find(
+      record => record[1].username === user.username
+    ), foundUser => foundUser ? bcrypt.compare(
+      user.password, foundUser[1].password,
+      (err, similar) => similar
+      ? withAs([nanoid(), +(new Date())], misc => jsonDB.set(
+        'users', foundUser[0],
+        {...foundUser[1], token: misc[0], lastLogin: misc[1]},
+        res => cb({status: true, token: misc[0]})
+      )) : cb({status: false, msg: 'Incorrect password.'})
+    ) : cb({status: false, msg: 'User not found.'}))
+  )),
+
+  socket.on('signout', (user, cb) => jsonDB.all(
+    'users', allUsers => withAs(
+      Object.entries(allUsers).find(record => ands([
+        record[1].username === user.username,
+        record[1].token === user.token
+      ])), foundUser => foundUser ? jsonDB.set(
+        'users', foundUser[0], {...foundUser[1], token: 0},
+        res => cb({status: true, msg: 'Sign out successful.'})
+      ) : cb({status: false, msg: 'Incorrect token.'})
+    )
+  )),
+
+  socket.on('grantAccess', (admin, user, cb) => jsonDB.all(
+    'users', usersJSON => withAs(
+      Object.entries(usersJSON), allUsers => withAs(
+        allUsers.find(rec => ands([
+          rec[1].access.includes('superadmin'),
+          rec[1].username === admin.username,
+          rec[1].token === admin.token
+        ])), checkAdmin => checkAdmin ? bcrypt.compare(
+          admin.password, checkAdmin[1].password,
+          (err, correctPass) => correctPass ? withAs(
+            allUsers.find(rec => rec[1].username === user.username),
+            grantedUser => grantedUser ? jsonDB.set(
+              'users', grantedUser[0],
+              {...grantedUser[1], access: user.access},
+              res => cb({status: true, msg: 'Access granted.'})
+            ) : cb({status: false, msg: 'User not found.'})
+          ) : cb({status: false, msg: 'Incorrect password.'})
+        ) : cb({status: false, msg: 'Incorrect credentials.'})
+      )
+    )
+  ))
+
+])
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const fs = require('fs'),
-io = require('socket.io'),
-{nanoid} = require('nanoid'),
+const
 express = require('express'),
-bcrypt = require('bcryptjs'),
-jsonDB = require('./deps/jsonDB.js'),
+userMgtSys = require('./deps/ums.js'),
 
-withAs = (obj, cb) => cb(obj),
 ands = arr => arr.reduce(
   (acc, inc) => acc && inc, true
 ),
@@ -14,67 +10,4 @@ app = express()
   .use(express.static('public'))
   .listen(3000)
 
-io(app).on('connection', socket => [
-
-  /* -------------- User Management System --------------- */
-
-  socket.on('signup', (user, cb) => jsonDB.all(
-    'users', allUsers => Object.entries(allUsers).find(
-      record => record[1].username === user.username
-    ) ? cb({status: false, msg: 'User already registered.'})
-    : bcrypt.hash(`${user.password}`, 10, (err, hash) => withAs(
-      nanoid(), id => jsonDB.set('users', id, {
-        ...user, id, password: hash, access: []
-      }, res => cb({status: true, msg: 'Registration successful.'}))
-    ))
-  )),
-
-  socket.on('signin', (user, cb) => jsonDB.all(
-    'users', allUsers => withAs(Object.entries(allUsers).find(
-      record => record[1].username === user.username
-    ), foundUser => foundUser ? bcrypt.compare(
-      user.password, foundUser[1].password,
-      (err, similar) => similar
-      ? withAs([nanoid(), +(new Date())], misc => jsonDB.set(
-        'users', foundUser[0],
-        {...foundUser[1], token: misc[0], lastLogin: misc[1]},
-        res => cb({status: true, token: misc[0]})
-      )) : cb({status: false, msg: 'Incorrect password.'})
-    ) : cb({status: false, msg: 'User not found.'}))
-  )),
-
-  socket.on('signout', (user, cb) => jsonDB.all(
-    'users', allUsers => withAs(
-      Object.entries(allUsers).find(record => ands([
-        record[1].username === user.username,
-        record[1].token === user.token
-      ])), foundUser => foundUser ? jsonDB.set(
-        'users', foundUser[0], {...foundUser[1], token: 0},
-        res => cb({status: true, msg: 'Sign out successful.'})
-      ) : cb({status: false, msg: 'Incorrect token.'})
-    )
-  )),
-
-  socket.on('grantAccess', (admin, user, cb) => jsonDB.all(
-    'users', usersJSON => withAs(
-      Object.entries(usersJSON), allUsers => withAs(
-        allUsers.find(rec => ands([
-          rec[1].access.includes('superadmin'),
-          rec[1].username === admin.username,
-          rec[1].token === admin.token
-        ])), checkAdmin => checkAdmin ? bcrypt.compare(
-          admin.password, checkAdmin[1].password,
-          (err, correctPass) => correctPass ? withAs(
-            allUsers.find(rec => rec[1].username === user.username),
-            grantedUser => grantedUser ? jsonDB.set(
-              'users', grantedUser[0],
-              {...grantedUser[1], access: user.access},
-              res => cb({status: true, msg: 'Access granted.'})
-            ) : cb({status: false, msg: 'User not found.'})
-          ) : cb({status: false, msg: 'Incorrect password.'})
-        ) : cb({status: false, msg: 'Incorrect credentials.'})
-      )
-    )
-  ))
-
-])
+userMgtSys(app)
